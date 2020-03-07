1224ab0bd256626ad5dcc4c67da4a70839d3b157
Peter Watkins <pdwatkins@me.com>
2020-03-07 06:02:33 +0000

    Learning one

--- things.py
+++ things.py
@@ -1,15 +1,13 @@
-# examples/things.py
+# things.py
 
 # Let's get this party started!
-from wsgiref.simple_server import make_server
-
 import falcon
 
 
 # Falcon follows the REST architectural style, meaning (among
 # other things) that you think in terms of resources and state
 # transitions, which map to HTTP verbs.
-class ThingsResource:
+class ThingsResource(object):
     def on_get(self, req, resp):
         """Handles GET requests"""
         resp.status = falcon.HTTP_200  # This is the default status
@@ -18,20 +16,11 @@ class ThingsResource:
                      '\n'
                      '    ~ Immanuel Kant\n\n')
 
-
 # falcon.API instances are callable WSGI apps
-# in larger applications the app is created in a separate file
-app = falcon.App()
+app = falcon.API()
 
 # Resources are represented by long-lived class instances
 things = ThingsResource()
 
 # things will handle all requests to the '/things' URL path
 app.add_route('/things', things)
-
-if __name__ == '__main__':
-    with make_server('', 8000, app) as httpd:
-        print('Serving on port 8000...')
-
-        # Serve until process is killed
-        httpd.serve_forever()
